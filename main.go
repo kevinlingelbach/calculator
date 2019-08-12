6bbb3ed3d271ff3d03b9fa4d4f3f0b74f09cf96f
kevinlingelbach <kjlingelbach995@gmail.com>
2019-08-12 15:15:10 -0400

    Added addition

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("Welcome to the amazing calculator!")
+	fmt.Println("1. Add")
+	fmt.Println("2. Subtract")
+	fmt.Println("3. Multiply")
+	fmt.Println("4. Divide \n\n")
+	fmt.Print("Select a function:")
+
+	var selection int
+	fmt.Scan(&selection)
+
+	x, y := getNumbers()
+	fmt.Println(x, " + ", y, " = ", add(x, y))
+}
+
+func getNumbers() (int, int) {
+	fmt.Print("Enter number 1: ")
+	var num1 int
+	fmt.Scan(&num1)
+
+	fmt.Print("Enter number 2: ")
+	var num2 int
+	fmt.Scan(&num2)
+
+	return num1, num2
+}
+
+func add(x int, y int) int {
+	return x + y
 }
