ed50768a1ae97fe82f6a1be47c93fcd804f12fbc
kevinlingelbach <kjlingelbach995@gmail.com>
2019-08-16 12:58:03 -0400

    Add looping functionality and README.md

--- main.go
+++ main.go
@@ -3,43 +3,68 @@ package main
 import "fmt"
 
 func main() {
+	running := true
+
+	for running {
+		intro()
+
+		var selection int
+		fmt.Scan(&selection)
+
+		x, y := getNumbers()
+
+		if selection == 1 {
+			fmt.Printf("%d + %d = %d\n", x, y, add(x, y))
+		} else if selection == 2 {
+			fmt.Printf("%d - %d = %d\n", x, y, subtract(x, y))
+		} else if selection == 3 {
+			fmt.Printf("%d * %d = %d\n", x, y, multiply(x, y))
+		} else if selection == 4 {
+			fmt.Printf("%d / %d = %d\n", x, y, divide(x, y))
+		} else if selection == 5 {
+			fmt.Printf("%d mod %d = %d\n", x, y, modulus(x, y))
+		} else {
+			fmt.Printf("Not a possible selection.")
+		}
+
+		running = keepGoing()
+	}
+}
+
+func intro() {
 	fmt.Println("Welcome to the amazing calculator!")
 	fmt.Println("1. Add")
 	fmt.Println("2. Subtract")
 	fmt.Println("3. Multiply")
 	fmt.Println("4. Divide")
+	fmt.Println("5. Modulus")
 	fmt.Print("Select a function:")
-
-	var selection int
-	fmt.Scan(&selection)
-
-	x, y := getNumbers()
-
-	if selection == 1 {
-		fmt.Printf("%d + %d = %d\n", x, y, add(x, y))
-	} else if selection == 2 {
-		fmt.Printf("%d - %d = %d\n", x, y, subtract(x, y))
-	} else if selection == 3 {
-		fmt.Println("%d * %d = %d\n", x, y, multiply(x, y))
-	} else if selection == 4 {
-		fmt.Println("%d / %d = %d\n", x, y, divide(x, y))
-	}
-}
-
-func intro() {
-
 }
 
 func getNumbers() (int, int) {
-	fmt.Print("Enter number 1: ")
+	fmt.Print("Enter the first number: ")
 	var num1 int
 	fmt.Scan(&num1)
 
-	fmt.Print("Enter number 2: ")
+	fmt.Print("Enter the second number: ")
 	var num2 int
 	fmt.Scan(&num2)
 
 	return num1, num2
+}
+
+func keepGoing() bool {
+	going := true
+
+	fmt.Print("Do you want to go again? (y/n): ")
+	var response string
+	fmt.Scan(&response)
+
+	if response == "n" {
+		going = false
+	}
+
+	return going
 }
 
 func add(x int, y int) int {
@@ -56,4 +81,8 @@ func multiply(x int, y int) int {
 
 func divide(x int, y int) int {
 	return x / y
+}
+
+func modulus(x int, y int) int {
+	return x % y
 }
